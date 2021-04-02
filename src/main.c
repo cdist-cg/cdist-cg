a14a21e424c6742fe090921be74643379e3b567c
Dennis Camera <cdist@dtnr.ch>
2021-04-02 15:37:41 +0200

    Add debug builds

--- src/main.c
+++ src/main.c
@@ -1,3 +1,16 @@
+#include <stdio.h>
+
+#ifdef HAVE_CONFIG_H
+#include "../config.h"
+#else
+#error no config.h
+#endif
+
+#include "config.h"
+
 int main(int argc, char *argv[]) {
+#if DEBUG
+	printf("Debugging enabled.\n");
+#endif
 	return 0;
 }
