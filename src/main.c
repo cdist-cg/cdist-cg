85fb9f6dc08e034f192f8d3d4f7e32dcdee41706
Dennis Camera <cdist@dtnr.ch>
2021-04-17 22:12:48 +0200

    [main.c] Implement basic option processing

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <strings.h>
 #include <unistd.h>
+#include <getopt.h> /* HACK: getopt() declaration */
 
 #ifdef HAVE_CONFIG_H
 #include "../config.h"
@@ -9,6 +11,13 @@
 #endif
 
 #include "config.h"
+
+/* command declarations */
+
+extern void cmd_banner(FILE *outstream);
+
+
+/* types and static variables */
 
 enum cdist_cmd {
 	CMD_UNKNOWN,
@@ -22,21 +31,91 @@ enum cdist_cmd {
 	CMD_SCAN
 };
 
-/* command declarations */
+enum cdist_opt {
+	PARAM_CONFIG_FILE
+};
 
-extern void cmd_banner(FILE *outstream);
+static const char *EXECNAME = PACKAGE;  /* default */
+
+static const char *options[] = {
+	[PARAM_CONFIG_FILE] = NULL
+};
+
+/* help */
+
+void print_usage(FILE *outstream) {
+	fprintf(outstream, "usage: %s ...\n", EXECNAME);
+}
+
+void print_version(FILE *outstream) {
+	fprintf(outstream, "%s %s\n", PACKAGE, VERSION);
+}
+
+void print_help(FILE *outstream) {
+	print_usage(outstream);
+	fprintf(outstream, "\n");
+	print_version(outstream);
+	fprintf(outstream, "\n");
+	fprintf(
+		outstream,
+		"foo\n");
+}
 
 
 /* argument parsing */
 
-static unsigned int get_global_opts(int argc, char *argv[]) {
+static int get_global_opts(int argc, char *argv[], const char *options[]) {
 	/**
 	 * Consume global cdist-cg options.
+	 * Will update the `options` variable.
+	 *
+	 * Note that this function will begin parsing options from the first element
+	 * of argv!
 	 *
 	 * @returns the number of consumed arguments
 	 */
 
-	return 0;
+	extern char *optarg;
+	extern int optind;
+	extern int optopt;
+	extern int opterr;
+	extern int optreset;
+
+	int c, error = 0;
+
+	opterr = 0;
+	optreset = 1;
+	optind = 0;
+
+	while ((c = getopt(argc, argv, ":g:hV")) != -1) {
+		switch (c) {
+		case 'g':
+			options[PARAM_CONFIG_FILE] = optarg;
+			break;
+		case 'h':
+			print_help(stdout);
+			exit(EXIT_SUCCESS);
+		case 'V':
+			print_version(stdout);
+			exit(EXIT_SUCCESS);
+		case ':':
+			fprintf(stderr, "%s: option requires an argument -- %c\n", EXECNAME, optopt);
+			error = 1;
+			break;
+		case '?':
+		default:
+			fprintf(stderr, "%s: illegal option -- %c\n", EXECNAME, optopt);
+			error = 1;
+			break;
+		}
+	}
+
+	if (error) {
+		print_usage(stderr);
+		exit(EXIT_FAILURE);
+	}
+
+	return optind;
 }
 
 static int get_cdist_cmd(int argc, char *argv[], enum cdist_cmd *cmd) {
@@ -81,13 +160,15 @@ int main(int argc, char *argv[]) {
 	int apos = 1;
 	enum cdist_cmd command = CMD_UNKNOWN;
 
+	EXECNAME = argv[0];
+
 	if (argc < 1) return 1;
 
 #if DEBUG
 	printf("Debugging enabled.\n");
 #endif
 
-	apos += get_global_opts((argc - apos), &argv[apos]);
+	apos += get_global_opts((argc - apos), &argv[apos], options);
 
 	if ((argc - apos) <= 0) {
 		fprintf(stderr, "no command given.\n");
@@ -102,32 +183,29 @@ int main(int argc, char *argv[]) {
 		break;
 	case CMD_CONFIG:
 		fprintf(stderr, "config command is not yet implemented.\n");
-		return 1;
+		return EXIT_FAILURE;
 		break;
 	case CMD_INSTALL:
 		fprintf(stderr, "install command is not available in cdist-cg.\n");
-		return 1;
-		break;
+		return EXIT_FAILURE;
 	case CMD_INVENTORY:
-		fprintf(stderr, "inventory command is not yet implemented..\n");
-		return 1;
+		fprintf(stderr, "inventory command is not yet implemented.\n");
+		return EXIT_FAILURE;
 		break;
 	case CMD_PREOS:
 		fprintf(stderr, "preos is not available in cdist-cg.\n");
-		return 1;
-		break;
+		return EXIT_FAILURE;
 	case CMD_SHELL:
 		fprintf(stderr, "cdist shell is not available in cdist-cg.\n");
-		return 1;
-		break;
+		return EXIT_FAILURE;
 	case CMD_INFO:
-		fprintf(stderr, "info command is not yet implemented..\n");
-		return 1;
+		fprintf(stderr, "info command is not yet implemented.\n");
+		return EXIT_FAILURE;
 		break;
 	default:
 		fprintf(stderr, "unknown command: %s\n", argv[apos]);
-		return 1;
+		return EXIT_FAILURE;
 	}
 
-	return 0;
+	return EXIT_SUCCESS;
 }
